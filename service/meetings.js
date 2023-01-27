1d811cecfc5b05556aa57d45190590a2be4465bc
glebasikk <isachenko.gleb.01@mail.ru>
2023-01-27 17:58:14 +0300

    error handler

--- service/meetings.js
+++ service/meetings.js
@@ -1,12 +1,20 @@
 const meetings = require("../repository/meetings");
-
+const notFound = require("../errors/NotFound");
 
 class Meetings {
     async allMeetings() {
-        return await meetings.allMeetings();
+         let result = await meetings.allMeetings();
+         if (result == null){
+            throw new notFound("Database is empty");
+         }
+         return result
     }
     async findMeetingByID(data) {
-        return await  meetings.findMeetingByID(data);
+        let result = await meetings.findMeetingByID(data);
+         if (result == null){
+            throw new notFound("Meeting with this id does not exist");
+         }
+         return result
     }
     async delMeetingByID(data) {
         return await  meetings.delMeetingByID(data);
@@ -15,7 +23,11 @@ class Meetings {
         return await meetings.addMeeting(data);
     }
     async updateMeetingInfo(data) {
-        return await meetings.updateMeetingInfo(data);
+        let result = await meetings.updateMeetingInfo(data);
+         if (result == null){
+            throw new notFound("Meeting with this id does not exist");
+         }
+         return result
     }
     
 }
